c12b1cedaf0c4afaee3e2dc8093515da8a8b762f
devasish sai pothumudi <126960187+devasish27@users.noreply.github.com>
2024-07-02 15:25:29 +0530

    Create program0.7.c

---
+++ program0.7.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int harshad(a){
+	int x=a, s=0, d;
+
+  while(x){
+  d=x%10;
+  s= s+d;
+  x=x/10;
+  }
+  if(a%s == 0){
+    return 0
+      }
+  else{
+    return -1
+      }
+}
+
+int main(){
+}
